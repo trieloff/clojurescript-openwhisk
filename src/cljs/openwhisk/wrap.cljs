a10274a8bf0d5d8f7d3ac15987245901e4ec532a
Lars Trieloff <trieloff@adobe.com>
2017-12-19 18:56:53 +0000

    support wrapping without exporting - add wrap function - prepare dot release

--- src/cljs/openwhisk/wrap.cljs
+++ src/cljs/openwhisk/wrap.cljs
@@ -14,6 +14,10 @@
     (p/then o (fn [r] (p/resolved (clj->js r))))
     (clj->js o)))
 
+(defn wrap [cljsfunc]
+  "Wraps a ClojureScript function so that it can be used as OpenWhisk main function"
+  (fn [args] (clj-promise->js (cljsfunc (js->clj args :keywordize-keys true)))))
+
 (defn wrapfn [cljsfunc]
   "Wraps and exports a ClojureScript function as OpenWhisk main function"
-  (set! js/main (fn [args] (clj-promise->js (cljsfunc (js->clj args :keywordize-keys true))))))
+  (set! js/main (wrap cljsfunc)))
